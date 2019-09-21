333beef49e98e2649a508b8294c2d1c608e4f7b4
Adam Barker <barkeracode@gmail.com>
2019-09-21 10:55:07 -0600

    Add implementation for downloading the latest image.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 extern crate reqwest;
+extern crate serde_json;
+
+use serde_json::Value;
 
 pub struct Camera {
     pub addr: String,
@@ -7,7 +10,49 @@ pub struct Camera {
 
 #[derive(Debug)]
 pub enum Error {
-    HTTP(reqwest::Error)
+    HTTP(reqwest::Error),
+    StatusCode(String),
+    JSON(serde_json::Error),
+    Utf8(std::string::FromUtf8Error),
+    MessageError,
+    NotCaptured,
+}
+
+fn validate_response(res: &mut reqwest::Response) -> Result<Vec<u8>, Error> {
+    if !res.status().is_success() {
+        return Err(Error::StatusCode(format!("{}", res.status())));
+    }
+
+    let mut data = match res.content_length() {
+        Some(len) => Vec::with_capacity(len as usize),
+        None => Vec::new(),
+    };
+
+    res.copy_to(&mut data)?;
+    Ok(data)
+}
+
+fn validate_json_response(res: &str) -> Result<(), Error> {
+    let msg: Value = serde_json::from_str(res)?;
+
+    if let Value::Object(msg) = msg {
+        // verify msg["errCode"] == 200
+        if let Some(Value::Number(num)) = msg.get("errCode") {
+            if num.as_i64() != Some(200) {
+                if let Some(Value::String(errmsg)) = msg.get("errMsg") {
+                    return Err(Error::StatusCode(format!("{}", errmsg)));
+                } else {
+                    return Err(Error::StatusCode("bad errCode".to_string()));
+                }
+            }
+        } else {
+            return Err(Error::MessageError);
+        }
+    } else {
+        return Err(Error::MessageError);
+    }
+
+    Ok(())
 }
 
 impl Camera {
@@ -18,30 +63,120 @@ impl Camera {
         }
     }
 
-    fn post(&self, url: String) -> Result<(), Error> {
-        self.client.post(&url).send()?;
-        Ok(())
+    fn post(&self, url: String) -> Result<Vec<u8>, Error> {
+        let mut res = self.client.post(&url).send()?;
+        let data = validate_response(&mut res)?;
+
+        Ok(data)
+    }
+
+    fn get(&self, url: String) -> Result<Vec<u8>, Error> {
+        let mut res = self.client.get(&url).send()?;
+        let data = validate_response(&mut res)?;
+
+        Ok(data)
     }
 
     pub fn set_iso(&self, iso: usize) -> Result<(), Error> {
         unimplemented!();
     }
 
-    pub fn get_lastest_capture(&self) -> Result<Vec<u8>, Error> {
-        unimplemented!();
+    pub fn get_latest_capture(&self) -> Result<Vec<u8>, Error> {
+        // The image won't be out of the buffer for some time, so we may need
+        // to retry multiple times.
+        let mut tries = 0;
+        loop {
+            let res = self
+                .get(format!("http://{}/v1/photos/latest/info", self.addr))?;
+            let json_str = String::from_utf8(res)?;
+            validate_json_response(&json_str)?;
+
+            let latest_info: Value = serde_json::from_str(&json_str)?;
+            let latest_info = if let Value::Object(obj) = latest_info {
+                obj
+            } else {
+                return Err(Error::MessageError);
+            };
+
+            if let Some(Value::Bool(true)) = latest_info.get("captured") {
+                break;
+            } else {
+                tries += 1;
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+            }
+
+            if tries > 3 {
+                return Err(Error::NotCaptured);
+            }
+        }
+
+        // determine where the latest photo is
+        let res =
+            self.get(format!("http://{}/v1/photos/latest/info", self.addr))?;
+        let json_str = String::from_utf8(res)?;
+        validate_json_response(&json_str)?;
+
+        let latest_info: Value = serde_json::from_str(&json_str)?;
+        let latest_info = if let Value::Object(obj) = latest_info {
+            obj
+        } else {
+            return Err(Error::MessageError);
+        };
+
+        let dir = if let Some(Value::String(dir)) = latest_info.get("dir") {
+            dir
+        } else {
+            return Err(Error::MessageError);
+        };
+
+        let fname = if let Some(Value::String(file)) = latest_info.get("file")
+        {
+            file
+        } else {
+            return Err(Error::MessageError);
+        };
+
+        // download the file
+        let data = self.get(format!(
+            "http://{}/v1/photos/{}/{}",
+            self.addr, dir, fname
+        ))?;
+
+        Ok(data)
     }
 
     pub fn shutter_press(&self) -> Result<(), Error> {
-        self.post(format!("http://{}/v1/camera/shoot/start", self.addr))
+        let res =
+            self.post(format!("http://{}/v1/camera/shoot/start", self.addr))?;
+        let json_str = String::from_utf8(res)?;
+        validate_json_response(&json_str)?;
+
+        Ok(())
     }
 
     pub fn shutter_release(&self) -> Result<(), Error> {
-        self.post(format!("http://{}/v1/camera/shoot/finish", self.addr))
+        let res =
+            self.post(format!("http://{}/v1/camera/shoot/finish", self.addr))?;
+        let json_str = String::from_utf8(res)?;
+        validate_json_response(&json_str)?;
+        Ok(())
     }
 }
 
 impl From<reqwest::Error> for Error {
     fn from(e: reqwest::Error) -> Error {
         Error::HTTP(e)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Error {
+        Error::JSON(e)
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(e: std::string::FromUtf8Error) -> Error {
+        Error::Utf8(e)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,34 @@ extern crate pentaxctl;
 
 use pentaxctl::Camera;
 
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
     let camera = Camera::new("192.168.0.1".to_string());
 
-    // Take a one second exposure when in bulb mode.
-    camera.shutter_press()
-        .expect("could not press shutter");
-    sleep(Duration::from_millis(1000));
-    camera.shutter_release()
-        .expect("could not release shutter");
+    // Take an exposure when in bulb mode.
+    println!("Taking photo...");
+    camera.shutter_press().expect("could not press shutter (are you in Bulb?)");
+
+    sleep(Duration::from_millis(250));
+
+    camera.shutter_release().expect("could not release shutter");
+
+    println!("Downloading image...");
+    let image = camera
+        .get_latest_capture()
+        .expect("could not download image from camera");
+
+    println!("Image downloaded. Size: {} bytes.", image.len());
+
+    let mut wtr = BufWriter::new(
+        File::create("image.dng").expect("could not create file"),
+    );
+
+    wtr.write_all(&image).expect("could not save image");
+
+    println!("Image saved.");
 }
